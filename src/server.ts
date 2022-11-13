8d0430a2d973a020891367896ad69c03b42b047a
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-11-13 08:41:46 -0300

    :sparkles: setup: prisma

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,19 @@
+import { PrismaClient } from "@prisma/client";
 import Fastify from "fastify";
+
+const prisma = new PrismaClient({
+    log: ['query', 'error']
+});
 
 async function bootstrapi() {
     const fastify = Fastify({
         logger: true
     })
 
-    fastify.get('/pools/count', () => {
-        return {count: 0}
+    fastify.get('/pools/count', async () => {
+        const pools = await prisma.pool.count()
+
+        return {count: pools}
     })
 
     await fastify.listen({port: 3333})
