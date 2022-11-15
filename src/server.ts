70aaff44cada4de1cbec18d134b9accd3f1cd33e
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-11-15 11:47:40 -0300

    :sparkles: create: route - POST (pool)

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import cors from '@fastify/cors'
 import { PrismaClient } from "@prisma/client";
 import Fastify from "fastify";
+import ShortUniqueId from 'short-unique-id';
+import { z } from 'zod';
+
 
 const prisma = new PrismaClient({
     log: ['query', 'error']
@@ -20,8 +23,28 @@ async function bootstrapi() {
 
         return {count: pools}
     })
-
     fastify.get('/users/count', async () => {count: await prisma.user.count()})
+    fastify.get('/guesses/count', async () => {count: await prisma.guess.count()})
+    
+    fastify.post('/pools', async (request, reply) => {
+        const createPoolBody = z.object({
+            title: z.string()
+        })
+
+        const {title} = createPoolBody.parse(request.body);
+
+        const generate = new ShortUniqueId({length: 6})
+        const code = String(generate()).toUpperCase()
+        
+        await prisma.pool.create({
+            data: {
+                title,
+                code
+            }
+        })
+
+        return reply.status(201).send({code})
+    })
 
 await fastify.listen({port: 3333, /*host: '0.0.0.0'*/})
 }
