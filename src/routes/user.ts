1db1eec7b3dd826a393e02ecb2e5a734c3c68c60
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-11-30 22:18:53 -0300

    :recycle: create: route - users/count (routes)

---
+++ src/routes/user.ts
@@ -0,0 +1,6 @@
+import { FastifyInstance } from "fastify";
+import { prisma } from '../lib/prisma';
+
+export async function userRoutes(fastify: FastifyInstance){
+    fastify.get('/users/count', async () => await prisma.user.count() )
+}
