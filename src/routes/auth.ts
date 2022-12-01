13870bbf60f6c6027e9bed48efe79264d8af7fb9
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-11-30 22:32:02 -0300

    :recycle: create: route - get [me] (routes.auth)

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,5 +1,8 @@
 import { FastifyInstance } from 'fastify';
 import { authenticate } from '../plugins/authenticate';
 export async function authRoutes(fastify: FastifyInstance) {
-
+    fastify.get('/me', 
+  		{ onRequest: [authenticate] },
+    	async request => {return {user: request.user}}
+    )
 }
