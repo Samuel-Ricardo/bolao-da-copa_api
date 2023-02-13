5b50e699989b4aa13bc10480613d1dcfbd183725
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-02-13 20:23:53 -0300

    :heavy_plus_sign: update: deps

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -2,6 +2,7 @@ import { FastifyInstance } from 'fastify';
 import { z } from 'zod';
 import { prisma } from '../lib/prisma';
 import { authenticate } from '../plugins/authenticate';
+
 export async function authRoutes(fastify: FastifyInstance) {
     fastify.get('/me', 
   		{ onRequest: [authenticate] },
