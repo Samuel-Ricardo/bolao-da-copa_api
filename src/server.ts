c13a9aa3d7da75942e3acb2ba40336816cea860e
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-02-13 21:40:09 -0300

    :recycle: refact: startup

--- src/server.ts
+++ src/server.ts
@@ -1,50 +1,16 @@
-import cors from '@fastify/cors'
-import { PrismaClient } from "@prisma/client";
+import cors from '@fastify/cors';
 import Fastify from "fastify";
-import ShortUniqueId from 'short-unique-id';
-import { z } from 'zod';
-
-
-const prisma = new PrismaClient({
-    log: ['query', 'error']
-});
+import jwt from '@fastify/jwt';   
+import {ENV} from './config'
+import { routes } from './routes';
 
 async function bootstrapi() {
     const fastify = Fastify({
         logger: true
     })
 
-    await fastify.register(cors, {
-        origin: true // allowed domains
-    });
-
-    fastify.get('/pools/count', async () => {
-        const pools = await prisma.pool.count()
-
-        return {count: pools}
-    })
-    fastify.get('/users/count', async () => {return {count: await prisma.user.count()}})
-    fastify.get('/guesses/count', async () => {return {count: await prisma.guess.count()}})
-    
-    fastify.post('/pools', async (request, reply) => {
-        const createPoolBody = z.object({
-            title: z.string()
-        })
-
-        const {title} = createPoolBody.parse(request.body);
-
-        const generate = new ShortUniqueId({length: 6})
-        const code = String(generate()).toUpperCase()
-        
-        await prisma.pool.create({
-            data: {
-                title,
-                code
-            }
-        })
-
-        return reply.status(201).send({code})
-    })
+    await fastify.register(jwt, {secret: ENV.JWT_SECRET});
+    await fastify.register(routes)
 
 await fastify.listen({port: 3333, /*host: '0.0.0.0'*/})
 }
