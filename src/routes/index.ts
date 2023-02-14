5a4c0763d328bf6192f77d3fe1f472616751a787
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-02-13 21:38:32 -0300

    :recycle: create: index - routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,23 @@
+import { FastifyInstance } from 'fastify';
 export * from './user'
 export * from './auth'
 export * from './user'
 export * from './guess'
 export * from './pool'
 export * from './game'
+
+import {
+    authRoutes,
+    gameRoutes,
+    guessRoutes,
+    poolRoutes,
+    userRoutes
+} from '.'
+
+export async function routes(fastify: FastifyInstance) {
+    await fastify.register(poolRoutes)
+    await fastify.register(authRoutes)
+    await fastify.register(gameRoutes)
+    await fastify.register(guessRoutes)
+    await fastify.register(userRoutes)
+}
