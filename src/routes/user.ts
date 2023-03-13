b1710ddf792fffcf6bed8b7742665e6801eae47a
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-03-13 18:10:42 -0300

    :bug: fix: response - count (pool / user)

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -2,5 +2,5 @@ import { FastifyInstance } from "fastify";
 import { prisma } from '../lib/prisma';
 
 export async function userRoutes(fastify: FastifyInstance){
-    fastify.get('/users/count', async () => await prisma.user.count() )
+    fastify.get('/users/count', async () => {return {count: await prisma.user.count()}})
 }
