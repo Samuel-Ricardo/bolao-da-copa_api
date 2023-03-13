921831bbe11c1374ad1ddd79e3f2f3e1403e2518
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-03-13 18:00:36 -0300

    :gear: setup: cors (server.ts)

--- src/server.ts
+++ src/server.ts
@@ -9,10 +9,12 @@ async function bootstrapi() {
         logger: true
     })
 
+    await fastify.register(cors, {origin:true})
+
     await fastify.register(jwt, {secret: ENV.JWT_SECRET});
     await fastify.register(routes)
 
-await fastify.listen({port: 3333, host: '0.0.0.0'})
+await fastify.listen({port: 3333, /*host: '0.0.0.0'*/})
 }
 
 bootstrapi()
